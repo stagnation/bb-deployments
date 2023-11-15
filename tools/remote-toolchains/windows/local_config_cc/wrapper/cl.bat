a037925e427a7ddaab09622b9664657c6380b025
Nils Wireklint <nils@meroton.com>
2023-11-15 13:03:15 +0100

    Workaround the depfile .d handling
    Bazel's code is a little finicky, the checking can be disabled with
    --features=-parse_showincludes. But then builds will fail because the .d
    file is a not mandatory declared output file. And msvc does not create
    it. The code path to parse it will handle its absence, but when we skip
    the parsing the internal workaround is also bypassed. So the solution is
    to create the file, and then not parse it.

--- tools/remote-toolchains/windows/local_config_cc/wrapper/cl.bat
+++ tools/remote-toolchains/windows/local_config_cc/wrapper/cl.bat
@@ -1,5 +1,26 @@
 @echo off
 
 set msvcpath=C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\Tools\MSVC\14.29.30133\bin\HostX64\x64
+for %%a in (%*) do (
+    echo %%a | findstr /C:"/Fo" 1>nul
+    if errorlevel 1 (
+        REM echo ""
+    ) else (
+        REM echo found
+        set output=%%a
+    )
 
-"%msvcpath%"\\cl.exe %*
+)
+REM echo %*
+REM echo "output: " %output%
+
+set dotd=%output%
+REM echo "dotd:" %dotd%
+call set dotd=%%dotd:/Fo=%""%%%
+call set dotd=%%dotd:.obj=".d"%%
+REM echo "dotd:" %dotd%
+
+REM copy /y NUL %dotd% >NUL
+echo . 2> %dotd% >NUL
+
+"%msvcpath%"\\cl.exe %* >NUL
